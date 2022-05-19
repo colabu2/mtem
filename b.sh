3c4fd8afe953486714538d202234afa6be0b7263
colabu2 <101911117+colabu2@users.noreply.github.com>
2022-05-19 23:35:45 +0800

    adasa

--- b.sh
+++ b.sh
@@ -1,3 +1,3 @@
 wget https://raw.githubusercontent.com/colabu2/ngrok/main/ngrok
 chmod 777 ngrok
-./ngrok authtoken 1l3Ba800x6BiId1L9uQi2IEl26G_TKGmgnbSVNxPmMo1pfPd
+./ngrok authtoken 1siBdJPw9qpiMHu8LmpTyxgGlW4_8BxxSypL9Wkv2ypwpFhv
