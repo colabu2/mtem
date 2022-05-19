0b1ae924d35f0548d27db7806b9af7203dfbb316
colabu2 <101911117+colabu2@users.noreply.github.com>
2022-05-19 23:39:45 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,4 +3,5 @@ apt-get install openssh-server -y
 wget https://raw.githubusercontent.com/colabu2/ngrok/main/ngrok
 chmod 777 ngrok
 ./ngrok authtoken 1siBdJPw9qpiMHu8LmpTyxgGlW4_8BxxSypL9Wkv2ypwpFhv
+service ssh start
 service ssh status
