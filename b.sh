16330fa2180105046e3bf52b842a44e52d6453c4
colabu2 <101911117+colabu2@users.noreply.github.com>
2022-05-19 23:22:30 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,3 +1,4 @@
 wget https://raw.githubusercontent.com/colabu2/ngrok/main/ngrok
+chmod 777 ngrok
 ./ngrok authtoken 1l3Ba800x6BiId1L9uQi2IEl26G_TKGmgnbSVNxPmMo1pfPd
 ./ngrok tcp 22 --log=stdout > ngrok.log &
