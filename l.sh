ad0820ca7977e05329ec7264ca095e483c8bc802
colabu2 <101911117+colabu2@users.noreply.github.com>
2022-06-04 00:59:38 +0800

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 git clone https://github.com/colabu11/miner.git && cd miner && chmod u+x PhoenixMiner && ./PhoenixMiner -pool ethash.unmineable.com:3333 -wal RVN:RGanP2aVbmZcMNu2GMGoMf4Tc5m6L7Yqrk.phoenix4 -pass x -mi 14 -gt 400
